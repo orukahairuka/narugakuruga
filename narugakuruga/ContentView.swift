697af1eddd2659cbc66fcceb9759d4f5ab958846
irukahaoruka <erika03301109@gmail.com>
2025-03-13 23:17:58 +0900

    .gitignoreを追加

---
+++ narugakuruga/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  narugakuruga
+//
+//  Created by 櫻井絵理香 on 2025/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ narugakuruga/narugakurugaApp.swift
@@ -0,0 +1,17 @@
+//
+//  narugakurugaApp.swift
+//  narugakuruga
+//
+//  Created by 櫻井絵理香 on 2025/03/13.
+//
+
+import SwiftUI
+
+@main
+struct narugakurugaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
