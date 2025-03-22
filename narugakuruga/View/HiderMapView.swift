52127aac6fd5b7bb7b1e0a58b6c994d5a0f49e1b
mafyyu <morita0126m@gmail.com>
2025-03-22 13:21:42 +0900

    ファイる分けた

---
+++ narugakuruga/View/HiderMapView.swift
@@ -0,0 +1,16 @@
+//
+//  a.swift
+//  narugakuruga
+//
+//  Created by 森田将嵩 on 2025/03/22.
+//
+
+import SwiftUI
+
+struct HiderMapView: View {
+    var body: some View {
+        VStack {
+            Text("隠れる側のマップ")
+        }
+    }
+}
